a53b01717546ea645d0a2a51c79dd92d6fd947d1
MohammedAnasuddin <connect.anasuddin@gmail.com>
2025-05-13 21:06:26 +0530

    Understood DOM get Methods

---
+++ JavaScript/DOM/get_Functions.js
@@ -0,0 +1,18 @@
+//. getElementById(id)  -> It's Id not ID 
+//> gets all elemnts having the specified id else null
+//Note: the parameter id is case sensitive
+//- If multiple elemnts with ame id's are  found it returns the first one
+
+
+//. getElementsByClassName(class1 class2) 
+//> gets all elements consiting of specified class names
+//- returns a live array consiting of matched html array 
+//- this aray is updated as the DOM changes
+//> elements can be accessed via indexing starting from 0
+
+
+//.getElementsByTagName(tag_name)
+//> returns the elements of specified type
+//- tag refers to HTML tag name
+//> returns a real-timr array which upadates as per DOM changes
+
