64af2afbf533e5d926cd88019a009fbecfc567f0
MohammedAnasuddin <141658651+MohammedAnasuddin@users.noreply.github.com>
2025-06-04 01:08:06 +0530

    Started BOM
    BOM - window object
     - innerWidth and innerHeight
    - open() and close()
    - Resizing windows
    - scroll

--- JavaScript/temp.js
+++ JavaScript/temp.js
@@ -1,14 +1,5 @@
-
-
-// . Creating a car
-
- buildCar(type, 
-    
-    function (type, function(){}){
-    console.log("Started process for car",type)
-
-
-}, 
-
-)
-
+let html = '';
+for (let i = 1; i <= 300; i++) {
+  html += `<li style="height:1px; border-bottom:1px solid #ccc; font-size:12px; text-align:center;">${i}</li>\n`;
+}
+console.log(html);
